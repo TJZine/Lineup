2d45d9e380f56fe2974007fc7cc85cd21ca5b251
Tristan Zine <zine96@protonmail.com>
2026-04-18 15:05:19 -0400

    refactor(settings): remove leaked settings-store barrel export

--- src/modules/ui/settings/index.ts
+++ src/modules/ui/settings/index.ts
@@ -5,7 +5,6 @@
  */
 
 export { SettingsScreen } from './SettingsScreen';
-export { SettingsStore } from './SettingsStore';
 export { createSettingsToggle } from './SettingsToggle';
 export { createSettingsSelect } from './SettingsSelect';
 export { SETTINGS_STORAGE_KEYS, DEFAULT_SETTINGS } from './constants';
